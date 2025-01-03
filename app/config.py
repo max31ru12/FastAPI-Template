5726ed230b8da2e7cfb675d3b2ae21b8f8fb9e18
max31ru12 <maxevg72@gmail.com>
2025-01-03 20:01:18 +0300

    chg: Изменил настройки

--- app/config.py
+++ app/config.py
@@ -1,20 +1,14 @@
 from os import getenv
+from pathlib import Path
 
 from dotenv import load_dotenv
+from pydantic_settings import BaseSettings
 
 load_dotenv()
 
 DEV_MODE: bool = getenv("DEV_MODE", "true").strip().lower() in {"true", "1", "yes"}
 
-DB_HOST: str = getenv("DB_HOST", default="postgres")
-DB_PORT: str = getenv("DB_PORT", default="5432")
-DB_PASSWORD: str = getenv("DB_PASSWORD", default="test")
-DB_USER: str = getenv("DB_USER", default="test")
-DB_NAME: str = getenv("DB_NAME", default="test")
-
-DB_URL: str = (
-    f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-)
+BASE_DIR = Path(__file__).parent.parent
 
 CONVENTION = {
     "ix": "ix_%(column_0_label)s",
@@ -23,3 +17,20 @@ CONVENTION = {
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
     "pk": "pk_%(table_name)s",
 }
+
+
+class Settings(BaseSettings):
+
+    DB_HOST: str = "localhost"
+    DB_PORT: str = "5432"
+    DB_PASSWORD: str = "test"
+    DB_USER: str = "test"
+    DB_NAME: str = "test"
+
+    class ConfigDict:
+        env: Path = BASE_DIR / ".env"
+
+
+settings = Settings()
+
+DB_URL: str = f"postgresql+asyncpg://{settings.DB_USER}:{settings.DB_PASSWORD}@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}"
